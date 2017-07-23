2782d200a774bf3644a122c6d0ec04581f7e4ea7
YunYouJun <910426929@qq.com>
2017-07-23 23:08:44 +0800

    Finish layout and routes

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,15 @@
+<div class="col-md-12">
+  <footer class="footer">
+    <small class="slogon">
+      <img class="brand-icon" src="https://dn-phphub.qbox.me/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34">
+      <a href="http://yunyoujun.cn/">
+        云游君 | 幻星科幻协会
+      </a>
+    </small>
+    <nav>
+      <ul>
+        <li><a href="{{ route('about') }}">关于</a></li>
+      </ul>
+    </nav>
+  </footer>
+</div>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,17 @@
 @section('title', '主页')
 
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>你好，幻想者！</h1>
+    <p class="lead">
+      幻星科幻协会 - Fantasy Star  &nbsp;
+      <a href="https://fantasystar.yunyoujun.cn">https://fantasystar.yunyoujun.cn</a>
+    </p>
+    <p>
+      幻想，从此处起航。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">注册</a>
+    </p>
+  </div>
 @stop

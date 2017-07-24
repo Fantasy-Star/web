4eb0a7d0d692bf1d82954381cbfa5699bdcf729c
YunYouJun <910426929@qq.com>
2017-07-24 16:33:19 +0800

    Finish user signup

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,10 @@
     <link rel="stylesheet" href="/css/app.css">
   </head>
   <body>
-    @include('layouts._header')
+      @include('layouts._header')
 
     <div class="container">
+      @include('shared.messages')
       @yield('content')
       @include('layouts._footer')
     </div>
